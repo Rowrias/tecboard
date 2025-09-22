6f8d6ec599ba06e6c11fb8415272a918ada26558
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2025-09-22 14:03:50 -0300

    05.Finalizando o projeto
    - Grid de eventos
    - Renderização condicional

--- src/App.jsx
+++ src/App.jsx
@@ -49,27 +49,33 @@ function App() {
 
   return (
     <main>
-
       <header>
         <img src="/logo.png" alt="" />
       </header>
-
       <Banner />
-
       <FormularioDeEvento temas={temas} aoSubmeter={adicionarEvento} />
-
-      {temas.map(function (item) {
-        return (
-          <section key={item.id}>
-            <Tema tema={item} />
-            {eventos.map(function (item, indice) {
-
-              return <CardEvento evento={item} key={indice} />
-            })}
-          </section>
-        )
-      })}
-
+      <section className="container">
+        {temas.map(function (tema) {
+          if (!eventos.some(function(evento) {
+            return evento.tema.id == tema.id
+          })) {
+            return null
+          }
+          return (
+            <section key={tema.id}>
+              <Tema tema={tema} />
+              <div className="eventos">
+                {eventos.filter(function (evento) {
+                  return evento.tema.id == tema.id
+                })
+                .map(function (evento, indice) {
+                  return <CardEvento evento={evento} key={indice} />
+                })}
+              </div>
+            </section>
+          )
+        })}
+      </section>
     </main>
   )
 }
